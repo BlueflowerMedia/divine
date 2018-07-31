e82c0561e900ce7d00cfcc40719041c903aa4317
A Akbar <aa2020s@yahoo.com>
2018-07-31 14:16:05 -0400

    services modified

--- page-services.php
+++ page-services.php
@@ -13,7 +13,6 @@
         <li><a id="item-wedding" href="#section-fine-art">Fine Art Reproduction</a></li>
         <li><a id="item-wedding" href="#section-photo-restoration">Photo Restoration</a></li>
         <li><a id="item-wedding" href="#section-modelling">Modelling</a></li>
-
   		</ul>
     </div>
 
@@ -21,17 +20,17 @@
 </section>
 
 <section id="section-professional">
-  <div class="container pro-top">
+  <div class="container box-part-top">
     <h1>Professional Portraits</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/IMG_0020 copy.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/IMG_484.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/Nabeel Khan.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
@@ -39,83 +38,83 @@
 
 
 <section id="section-id">
-  <div class="container id-top">
+  <div class="container box-part-top">
     <h1>ID Photos</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2343.jpg.jpg" alt="">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3823.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2349.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2999 copy.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3048.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3549 copy.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3620.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3623.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3823.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2343.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
 </section>
 
 <section id="section-family">
-  <div class="container family-top">
+  <div class="container box-part-top">
     <h1>Family Portraits</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/FamilyPhoto/IMG_6174 Khan 2.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/FamilyPhoto/Nabeel Khan.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
 </section>
 
 <section id="section-wedding">
-  <div class="container wedding-top">
+  <div class="container box-part-top">
     <h1>Wedding Photography</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0005.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0016.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0067 copy.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
 </section>
 
 <section id="section-fine-art">
-  <div class="container fine-art-top">
+  <div class="container box-part-top">
     <h1>Fine Art Reproduction</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/FineArtRro/IMG_0075.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/FineArtRro/Nabeel Khan KSA185.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
 </section>
 
 <section id="section-photo-restoration">
-  <div class="container photo-restoration-top">
+  <div class="container box-part-top">
     <h1>Photo Restoration</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/ Destop433.jpg" alt="">
       <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Amin.jpg" alt=""> -->
       <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Divine 3507.jpg" alt="">
@@ -126,19 +125,19 @@
       <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_1207 RAW.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Urmi.jpg" alt="">
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
 </section>
 
 <section id="section-modelling">
-  <div class="container modelling-top">
+  <div class="container box-part-top">
     <h1>Modelling</h1>
   </div>
 
-	<div class="container part-down flex-row">
-    <div class="part-pics flex1">
+	<div class="container box-part-down flex-row">
+    <div class="box-part-pics flex1">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Destop824.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Divine.jpg" alt="">
       <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/IMG_0192.jpg" alt="">
@@ -152,7 +151,7 @@
       <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 6.jpg" alt="">
 
     </div>
-    <div class="part-txt flex1">
+    <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 	</div>
