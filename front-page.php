a7d73c6211c1169acedddcdd05e03c00bce2d69c
A Akbar <aa2020s@yahoo.com>
2018-08-08 15:53:28 -0400

    99.91% complete

--- front-page.php
+++ front-page.php
@@ -22,16 +22,19 @@
 
 				<div class="photo-wrapper-slider flex1 row owl-carousel">
 					<div>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/placeholders/1.jpg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/homepage/home-1.jpg" alt="">
 					</div>
 					<div>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/placeholders/2.jpg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/homepage/home-2.jpg" alt="">
 					</div>
 					<div>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/placeholders/3.jpg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/homepage/home-3.jpg" alt="">
 					</div>
 					<div>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/placeholders/4.jpg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/homepage/home-4.jpg" alt="">
+					</div>
+					<div>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/homepage/home-5.jpg" alt="">
 					</div>
 			  </div>
 
--- page-services.php
+++ page-services.php
@@ -26,9 +26,12 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/IMG_0020 copy.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/IMG_484.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/ProfessionalPortraits/Nabeel Khan.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-4.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-5.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-6.jpg" alt="">
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -44,14 +47,11 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3823.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2349.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2999 copy.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3048.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3549 copy.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3620.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_3623.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/idPhoto/IMG_2343.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-id-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-id-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-id-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-id-4.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-id-5.jpg" alt="">
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -66,8 +66,12 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/FamilyPhoto/Nabeel Khan.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/FamilyPhoto/IMG_6174 Khan 2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-4.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-5.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-portrait-6.jpg" alt="">
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -82,9 +86,10 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0005.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0016.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Wedding/IMG_0067 copy.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-wedding-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-wedding-2.jpg" alt="">
+
+
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -99,17 +104,12 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Destop824.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Divine.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/IMG_0192.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/IMG_3213 copy 1.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/IMG_3215 copy.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 1.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 2.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 3.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 4.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 5.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/Modelling/Mim 6.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-modelling-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-modelling-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-modelling-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-modelling-4.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-modelling-5.jpg" alt="">
+
 
     </div>
     <div class="box-part-txt flex1">
@@ -126,8 +126,11 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/FineArtRro/IMG_0075.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/FineArtRro/Nabeel Khan KSA185.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-fineart-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-fineart-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-fineart-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-fineart-4.jpg" alt="">
+
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -142,15 +145,10 @@
 
 	<div class="container box-part-down flex-row">
     <div class="box-part-pics flex1">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/ Destop433.jpg" alt="">
-      <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Amin.jpg" alt=""> -->
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Divine 3507.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_0013.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_0020.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_0025.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_0051.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/IMG_1207 RAW.jpg" alt="">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/PhotoRestoration/Urmi.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-restoration-1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-restoration-2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-restoration-3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/services/services-restoration-4.jpg" alt="">
     </div>
     <div class="box-part-txt flex1">
       <p class="p-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
