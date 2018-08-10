884ef73f09591a3adf08894b1574be41803a81c1
Fahim <Fahim@Fahims-Hut.local>
2018-08-09 20:22:31 -0400

    portfolio update

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 
  $aboutme = get_permalink(get_page_by_path( 'aboutme' ));
- $portfolio = get_permalink(get_page_by_path( 'Portfolio' ));
+ $portfolio = get_permalink(get_page_by_path( 'portfolio/photos' ));
  $services = get_permalink(get_page_by_path( 'services' ));
  $contact = get_permalink(get_page_by_path( 'contact' ));
 
